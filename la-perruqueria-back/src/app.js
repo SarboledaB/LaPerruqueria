7219d51cb671ac75f7b3eaba266e09b90cddd790
Sebastian Arboleda <sarboledabotero@gmail.com>
2025-07-04 23:24:59 -0500

    cors

--- la-perruqueria-back/src/app.js
+++ la-perruqueria-back/src/app.js
@@ -8,15 +8,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
-
-const corsOptions = {
+app.use(cors({
   origin: process.env.CLIENT_URL,
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
-};
+  credentials: true
+}));
 
-app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
